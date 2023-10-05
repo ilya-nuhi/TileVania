b0a515896aeceb54f26f8cf30ff0f804d8c8a83b
ilya-nuhi <nuhi18@itu.edu.tr>
2023-10-05 03:08:15 +0300

    enemy face flipping added

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,21 +9,36 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] float movementSpeed = 1f;
     Rigidbody2D myRigidBody;
     BoxCollider2D myBoxCollider;
+    CircleCollider2D myCircleCollider;
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myBoxCollider = GetComponent<BoxCollider2D>();
+        myCircleCollider = GetComponent<CircleCollider2D>();
     }
 
     void Update()
     {
         myRigidBody.velocity = new Vector2 (movementSpeed,0f);
+        FlipEnemyFacingHelper();
     }
 
-    void OnTriggerExit2D(Collider2D other) {
-        movementSpeed = -movementSpeed;
-        FlipEnemyFacing();
+    void FlipEnemyFacingHelper()
+    {
+        if(myBoxCollider.IsTouchingLayers(LayerMask.GetMask("Hazard","Ground"))){
+            movementSpeed = -movementSpeed;
+            FlipEnemyFacing();
+        }
+        if(!myCircleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            movementSpeed = -movementSpeed;
+            FlipEnemyFacing();
+        }
     }
+
+    // void OnTriggerExit2D(Collider2D other) {
+    //     movementSpeed = -movementSpeed;
+    //     FlipEnemyFacing();
+    // }
 
     void FlipEnemyFacing()
     {
